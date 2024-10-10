4f996a03949f2c664f0c28a40609bdc174023dbf
Likhit Sakhare <sakhare1181likhit@gmail.com>
2024-10-10 17:09:11 +0530

    final merge change 2

--- app/src/main/java/com/likhit/dummygit/MainActivity.kt
+++ app/src/main/java/com/likhit/dummygit/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         println("THIS IS CONTAINED IN MY SECOND COMMIT")
         println("LOCAL COMMIT")
-        println("CHANGE 2")
+        println("CHANGE 4")
         println("THIS LINE WILL BE MERGED")
     }
 }
